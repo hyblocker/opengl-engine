cae05a7e6e8af36a6d721202f165431e88da2155
Hyblocker <hyblocker@protonmail.com>
2024-11-03 13:11:40 +0100

    (chore): Project structure

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "engine/app.hpp"
+#include "game_layer.hpp"
+
+int main (int argc, char *argv[]) {
+	App app({
+		.window = {
+			.width = 1280,
+			.height = 720,
+			.title = "OpenGL Engine"
+		}
+		});
+
+	GameLayer* gameLayer = new GameLayer(app.getDeviceManager());
+	app.pushLayer(gameLayer);
+
+	app.run();
+
+	delete gameLayer;
+}
