daf62f25153bf5244bd2c0345b3cc3b317b632e5
Hyblocker <hyblocker@protonmail.com>
2024-11-03 13:13:32 +0100

    (feat): Add layer system

---
+++ src/engine/ilayer.cpp
@@ -0,0 +1,2 @@
+#include "ilayer.hpp"
+
---
+++ src/engine/ilayer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <inttypes.h>
+#include "gpu/device_manager.hpp"
+
+namespace engine {
+    class ILayer {
+    public:
+        explicit ILayer(gpu::DeviceManager* deviceManager)
+            : m_deviceManager(deviceManager)
+        {}
+        virtual ~ILayer() = default;
+
+        virtual void update(double timeElapsed, double deltaTime) { }
+        virtual void render(double deltaTime) { }
+        virtual void backBufferResizing() { }
+        virtual void backBufferResized(uint32_t width, uint32_t height, uint32_t samples) { }
+
+        [[nodiscard]] gpu::DeviceManager* getDeviceManager() const { return m_deviceManager; }
+        [[nodiscard]] gpu::IDevice* getDevice() const { return m_deviceManager->getDevice(); }
+    private:
+        gpu::DeviceManager* m_deviceManager = nullptr;
+    };
+}
