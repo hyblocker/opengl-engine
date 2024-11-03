d44202c229f7695dd196053f2d4706535b448364
Hyblocker <hyblocker@protonmail.com>
2024-11-03 13:14:08 +0100

    (feat): Implement game layer (hello triangle)

---
+++ src/game_layer.cpp
@@ -0,0 +1,118 @@
+#include "game_layer.hpp"
+
+#include <glad/glad.h>
+
+struct PositionColorVertex {
+    float position[3];
+    float color[3];
+};
+
+#define SHADER_HEADER "#version 330 core"
+
+constexpr const char shader_vert[] = SHADER_HEADER R"(
+layout (location = 0) in vec3 iPosition;
+layout (location = 1) in vec3 iColor;
+
+out vec3 color;
+
+void main()
+{
+    gl_Position = vec4(iPosition.x, iPosition.y, iPosition.z, 1.0);
+    color = iColor;
+}
+)";
+constexpr const char shader_pixel[] = SHADER_HEADER R"(
+precision mediump float;
+
+in vec3 color;
+
+void main()
+{
+    gl_FragColor = vec4(color, 1.0f);
+} 
+)";
+
+PositionColorVertex vertices[3] = {
+    {{ -1, -1, 0 }, { 1, 0, 0 }},
+    {{  1, -1, 0 }, { 0, 1, 0 }},
+    {{  1,  1, 0 }, { 0, 0, 1 }}
+};
+uint16_t indices[3] = { 0, 1, 2 };
+
+GameLayer::GameLayer(gpu::DeviceManager* deviceManager)
+    : ILayer(deviceManager) {
+
+    getDevice()->setViewport({
+        .left = 0,
+        .right = App::getInstance()->getWindow()->getWidth(),
+        .top = 0,
+        .bottom = App::getInstance()->getWindow()->getHeight(),
+        });
+
+    glClearColor(0, 0, 0, 1);
+
+    // Vertex buffer
+    m_vertexBuffer = getDevice()->makeBuffer({ .type = gpu::BufferType::VertexBuffer, .usage = gpu::Usage::Default });
+    getDevice()->writeBuffer(m_vertexBuffer, sizeof(vertices), vertices);
+
+    // Triangle indices (connectivity)
+    m_indexBuffer = getDevice()->makeBuffer({ .type = gpu::BufferType::IndexBuffer, .usage = gpu::Usage::Default, .format = gpu::GpuFormat::Uint16_TYPELESS });
+    getDevice()->writeBuffer(m_vertexBuffer, sizeof(indices), indices);
+
+    // Make a shader but awesome
+    m_shader = getDevice()->makeShader({
+        .debugName = "triangle",
+        .VS { .byteCode = (uint8_t*) shader_vert, .entryFunc = "main" },
+        .PS { .byteCode = (uint8_t*) shader_pixel, .entryFunc = "main" },
+        .graphicsState = {
+            .depthState = gpu::COMPARE::GREATER_OR_EQUAL, // inverse Z
+            .vertexLayout = m_vertexLayout,
+    }   });
+
+    // VertexLayout
+    gpu::VertexAttributeDesc vDesc[2] = {
+        { .name = "POSITION", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 0, .offset = offsetof(PositionColorVertex, position), .elementStride = sizeof(PositionColorVertex)},
+        { .name = "COLOR", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 1, .offset = offsetof(PositionColorVertex, color), .elementStride = sizeof(PositionColorVertex)}
+    };
+    m_vertexLayout = getDevice()->createInputLayout(vDesc, sizeof(vDesc) / sizeof(vDesc[0]));
+
+    // Prepare cbuffer to populate it with transform matrices
+    m_cbufferData = {
+        // .model = hlslpp::float4x4::look_at(pos, target, up) :
+    };
+}
+
+GameLayer::~GameLayer() {
+}
+
+void GameLayer::update(double timeElapsed, double deltaTime) {
+
+}
+
+void GameLayer::render(double deltaTime) {
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Bind vertex buffer
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer->getNativeObject());
+    // Bind index buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer->getNativeObject());
+
+    // Show how to interpret vertex format attributes
+    glUseProgram(m_shader->getNativeObject());
+    glBindVertexArray(m_vertexLayout->getNativeObject());
+
+    // Draw
+    // DrawIndexed
+    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_SHORT, 0);
+    // Present
+    glFlush();
+}
+
+void GameLayer::backBufferResizing() {
+
+}
+
+void GameLayer::backBufferResized(uint32_t width, uint32_t height, uint32_t samples) {
+
+}
---
+++ src/game_layer.hpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "engine/app.hpp"
+#include <hlsl++.h>
+
+struct CBuffer {
+    hlslpp::float4x4 model;
+    hlslpp::float4x4 view;
+    hlslpp::float4x4 projection;
+};
+
+class GameLayer : public engine::ILayer {
+public:
+    GameLayer(gpu::DeviceManager* deviceManager);
+    ~GameLayer() override;
+
+    void update(double timeElapsed, double deltaTime) override;
+    void render(double deltaTime) override;
+    void backBufferResizing() override;
+    void backBufferResized(uint32_t width, uint32_t height, uint32_t samples) override;
+
+private:
+
+    CBuffer m_cbufferData;
+
+    gpu::BufferHandle m_vertexBuffer;
+    gpu::BufferHandle m_indexBuffer;
+    gpu::InputLayoutHandle m_vertexLayout;
+    gpu::ShaderHandle m_shader;
+
+    gpu::BufferHandle m_cbuffer;
+};
