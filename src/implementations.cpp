db2497e31458f5de6dd39ea978c0544b54020a58
Hyblocker <hyblocker@protonmail.com>
2025-01-01 15:21:14 +0100

    (chore): Implement imgui

--- src/implementations.cpp
+++ src/implementations.cpp
@@ -1,4 +1,19 @@
 // implement single header libs here
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb_image.h"]
+
+// imgui core lib
+#include <imgui.cpp>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
+
+// imgui helper
+#include <misc/cpp/imgui_stdlib.cpp>
+
+// imgui renderer
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include <backends/imgui_impl_opengl3.cpp>
+#include <backends/imgui_impl_glfw.cpp>
