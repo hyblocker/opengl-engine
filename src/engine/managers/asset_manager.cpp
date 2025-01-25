314a7006a0f07f74eb4fd0dd4f994deef601c149
Hyblocker <hyblocker@protonmail.com>
2025-01-25 22:23:44 +0100

    (feat): Asset Manager

--- src/engine/managers/asset_manager.cpp
+++ src/engine/managers/asset_manager.cpp
@@ -5,10 +5,71 @@
 
 #include <stb_image.h>
 #include <fmt/format.h>
+#include <tiny_obj_loader.h>
 #include <windows.h>
 #include <stb_include.h>
 
 #define SHADER_HEADER "#version 460 core\n\n"
+
+// @TODO: In a more production ready environment, replace this with a proper mesh, like the Source ERROR mesh
+render::PositionNormalTexcoordVertex errorVertices[] = {
+    // Front face
+    {.position = { -1, -1,  1 }, .normal = {  0,  0,  1 }, .uv = { 0, 0 }},
+    {.position = {  1, -1,  1 }, .normal = {  0,  0,  1 }, .uv = { 1, 0 }},
+    {.position = {  1,  1,  1 }, .normal = {  0,  0,  1 }, .uv = { 1, 1 }},
+    {.position = { -1,  1,  1 }, .normal = {  0,  0,  1 }, .uv = { 0, 1 }},
+
+    // Back face
+    {.position = {  1, -1, -1 }, .normal = {  0,  0, -1 }, .uv = { 0, 0 }},
+    {.position = { -1, -1, -1 }, .normal = {  0,  0, -1 }, .uv = { 1, 0 }},
+    {.position = { -1,  1, -1 }, .normal = {  0,  0, -1 }, .uv = { 1, 1 }},
+    {.position = {  1,  1, -1 }, .normal = {  0,  0, -1 }, .uv = { 0, 1 }},
+
+    // Left face
+    {.position = { -1, -1, -1 }, .normal = { -1,  0,  0 }, .uv = { 0, 0 }},
+    {.position = { -1, -1,  1 }, .normal = { -1,  0,  0 }, .uv = { 1, 0 }},
+    {.position = { -1,  1,  1 }, .normal = { -1,  0,  0 }, .uv = { 1, 1 }},
+    {.position = { -1,  1, -1 }, .normal = { -1,  0,  0 }, .uv = { 0, 1 }},
+
+    // Right face
+    {.position = {  1, -1,  1 }, .normal = {  1,  0,  0 }, .uv = { 0, 0 }},
+    {.position = {  1, -1, -1 }, .normal = {  1,  0,  0 }, .uv = { 1, 0 }},
+    {.position = {  1,  1, -1 }, .normal = {  1,  0,  0 }, .uv = { 1, 1 }},
+    {.position = {  1,  1,  1 }, .normal = {  1,  0,  0 }, .uv = { 0, 1 }},
+
+    // Top face
+    {.position = { -1,  1,  1 }, .normal = {  0,  1,  0 }, .uv = { 0, 0 }},
+    {.position = {  1,  1,  1 }, .normal = {  0,  1,  0 }, .uv = { 1, 0 }},
+    {.position = {  1,  1, -1 }, .normal = {  0,  1,  0 }, .uv = { 1, 1 }},
+    {.position = { -1,  1, -1 }, .normal = {  0,  1,  0 }, .uv = { 0, 1 }},
+
+    // Bottom face
+    {.position = { -1, -1, -1 }, .normal = {  0, -1,  0 }, .uv = { 0, 0 }},
+    {.position = {  1, -1, -1 }, .normal = {  0, -1,  0 }, .uv = { 1, 0 }},
+    {.position = {  1, -1,  1 }, .normal = {  0, -1,  0 }, .uv = { 1, 1 }},
+    {.position = { -1, -1,  1 }, .normal = {  0, -1,  0 }, .uv = { 0, 1 }},
+};
+
+uint32_t errorIndices[] = {
+    // Front face
+    0, 1, 2,  2, 3, 0,
+
+    // Back face
+    4, 5, 6,  6, 7, 4,
+
+    // Left face
+    8, 9, 10,  10, 11, 8,
+
+    // Right face
+    12, 13, 14,  14, 15, 12,
+
+    // Top face
+    16, 17, 18,  18, 19, 16,
+
+    // Bottom face
+    20, 21, 22,  22, 23, 20,
+};
+
 
 namespace managers {
     AssetManager::AssetManager(gpu::IDevice* device) 
@@ -40,7 +101,31 @@ namespace managers {
 
     void AssetManager::initialiseErrorData() {
 
+        m_device->debugMarkerPush(fmt::format("Initialising error data..."));
         // Initialises error mesh, error shader and texture
+
+        // Init errMesh
+        m_errorMesh.vertexBuffer = m_device->makeBuffer({ .type = gpu::BufferType::VertexBuffer, .usage = gpu::Usage::Default, .debugName = "ErrorVertexBuffer"});
+        m_device->writeBuffer(m_errorMesh.vertexBuffer, sizeof(errorVertices), errorVertices);
+
+        // Triangle indices
+        m_errorMesh.indexBuffer = m_device->makeBuffer({ .type = gpu::BufferType::IndexBuffer, .usage = gpu::Usage::Default, .format = gpu::GpuFormat::Uint32_TYPELESS, .debugName = "ErrorIndexBuffer"});
+        m_device->writeBuffer(m_errorMesh.indexBuffer, sizeof(errorIndices), errorIndices);
+
+        // Requires mesh to be initialised first
+        gpu::VertexAttributeDesc vDesc[] = {
+            {.name = "POSITION", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 0, .offset = offsetof(render::PositionNormalTexcoordVertex, position), .elementStride = sizeof(render::PositionNormalTexcoordVertex)},
+            {.name = "NORMAL", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 1, .offset = offsetof(render::PositionNormalTexcoordVertex, normal), .elementStride = sizeof(render::PositionNormalTexcoordVertex)},
+            {.name = "TEXCOORD0", .format = gpu::GpuFormat::RG8_TYPELESS, .bufferIndex = 2, .offset = offsetof(render::PositionNormalTexcoordVertex, uv), .elementStride = sizeof(render::PositionNormalTexcoordVertex)}
+        };
+
+        m_errorMesh.vertexLayout = m_device->createInputLayout(vDesc, sizeof(vDesc) / sizeof(vDesc[0]));
+        
+        // Mesh data
+        m_errorMesh.mesh.vertexBuffer = m_errorMesh.vertexBuffer;
+        m_errorMesh.mesh.indexBuffer = m_errorMesh.indexBuffer;
+        m_errorMesh.mesh.vertexLayout = m_errorMesh.vertexLayout;
+        m_errorMesh.mesh.triangleCount = (sizeof(errorIndices) / sizeof(errorIndices[0])) / 3;
 
         // Init errTex
         uint8_t texData[] = {
@@ -54,6 +139,7 @@ namespace managers {
             .height = (uint32_t)2,
             .generateMipmaps = false,
             .type = gpu::TextureType::Texture2D,
+            .debugName = "ErrorTexture"
             }, texData);
 
         // Init errShader
@@ -89,22 +175,16 @@ void main()
 } 
 )";
 
-        gpu::VertexAttributeDesc vDesc[] = {
-            {.name = "POSITION", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 0, .offset = offsetof(render::PositionColorVertex, position), .elementStride = sizeof(render::PositionColorVertex)},
-            {.name = "COLOR", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 1, .offset = offsetof(render::PositionColorVertex, color), .elementStride = sizeof(render::PositionColorVertex)},
-            {.name = "TEXCOORD0", .format = gpu::GpuFormat::RG8_TYPELESS, .bufferIndex = 2, .offset = offsetof(render::PositionColorVertex, uv), .elementStride = sizeof(render::PositionColorVertex)}
-        };
-        auto vertexLayoutHandle = m_device->createInputLayout(vDesc, sizeof(vDesc) / sizeof(vDesc[0]));
-
         // Make a shader but awesome
         m_errorShader = m_device->makeShader({
-            .debugName = "ErrorShader",
             .VS {.byteCode = (uint8_t*)shader_vert, .entryFunc = "main" },
             .PS {.byteCode = (uint8_t*)shader_pixel, .entryFunc = "main" },
             .graphicsState = {
                 .depthState = gpu::CompareFunc::LessOrEqual, // inverse Z
-                .vertexLayout = vertexLayoutHandle,
-        } });
+            },
+            .debugName = "ErrorShader",
+        });
+        m_device->debugMarkerPop();
 
         m_intialisedDefaultAssets = true;
     }
@@ -115,9 +195,149 @@ void main()
             initialiseErrorData();
         }
 
+        // Check if the shader was loaded before
+        std::string meshKey = meshPath;
+        if (m_meshes.find(meshKey) != m_meshes.end()) {
+            return m_meshes[meshKey].mesh;
+        }
+        
+        std::string filePathMesh = fmt::format("{}/assets/meshes/{}", m_applicationRootPath, meshPath);
 
+        // @HACK: This is super simple and poor for more sophisticated development
+        //        1. We should use a more sophisticated library like ASSIMP or a mixture of them to better support more widely
+        //           used mesh file formats such as Autodesk FBX and GLTF.
+        //        2. Dynamically determine the vertex layout based on the mesh data, currently this is hardcoded to only support position normal and UV0 data, what if I want to import tangent vectors or UV1?
+        //        3. Introduce a "baked" asset pipeline, where assets are pre-processed ahead of time from builds. This allows us to better optimise assets.
+        //        4. Use meshoptimizer. meshoptimizer is a library which better distributes the data to make it easier for the GPU to work with, and can generate LODs and similar too.
 
-        return m_errorMesh;
+        // Triangulate quads, ignore vertex colours and search for MTLs in the same dir as objs
+        tinyobj::ObjReaderConfig reader_config;
+        reader_config.triangulate = true;
+        reader_config.triangulation_method = "earcut";
+        reader_config.vertex_color = false;
+        reader_config.mtl_search_path = "";
+
+        tinyobj::ObjReader reader;
+
+        if (!reader.ParseFromFile(filePathMesh, reader_config)) {
+            if (!reader.Error().empty()) {
+                LOG_ERROR("TinyObjReader: {}", reader.Error());
+            }
+            LOG_WARNING("Failed to load mesh {}. Using error mesh...", meshPath);
+            return m_errorMesh.mesh;
+        }
+
+        if (!reader.Warning().empty()) {
+            LOG_WARN("TinyObjReader: {}", reader.Warning());
+        }
+
+        auto& attrib = reader.GetAttrib();
+        auto& shapes = reader.GetShapes();
+        auto& materials = reader.GetMaterials();
+
+        std::vector<render::PositionNormalTexcoordVertex> vertices;
+        std::vector<uint32_t> indices;
+
+        // Loop over shapes
+        for (size_t s = 0; s < shapes.size(); s++) {
+            // Loop over faces(polygon)
+            size_t index_offset = 0;
+            size_t index_buffer_offset = 0;
+            for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+                size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+
+                // Loop over vertices in the face.
+                for (size_t v = 0; v < fv; v++) {
+                    render::PositionNormalTexcoordVertex currentVertex{};
+
+                    // access to vertex
+                    tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+                    currentVertex.position[0] = attrib.vertices[3 * size_t(idx.vertex_index) + 0];
+                    currentVertex.position[1] = attrib.vertices[3 * size_t(idx.vertex_index) + 1];
+                    currentVertex.position[2] = attrib.vertices[3 * size_t(idx.vertex_index) + 2];
+
+                    // Check if `normal_index` is zero or positive. negative = no normal data
+                    if (idx.normal_index >= 0) {
+                        currentVertex.normal[0] = attrib.normals[3 * size_t(idx.normal_index) + 0];
+                        currentVertex.normal[1] = attrib.normals[3 * size_t(idx.normal_index) + 1];
+                        currentVertex.normal[2] = attrib.normals[3 * size_t(idx.normal_index) + 2];
+                    }
+
+                    // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+                    if (idx.texcoord_index >= 0) {
+                        currentVertex.uv[0] = attrib.texcoords[2 * size_t(idx.texcoord_index) + 0];
+                        currentVertex.uv[1] = attrib.texcoords[2 * size_t(idx.texcoord_index) + 1];
+                    }
+
+                    // Optional: vertex colors
+                    // tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
+                    // tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
+                    // tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
+
+                    vertices.push_back(currentVertex);
+                }
+                for (uint32_t i = 0; i < fv; i += 3) {
+                    indices.push_back(index_buffer_offset + i + 0);
+                    indices.push_back(index_buffer_offset + i + 1);
+                    indices.push_back(index_buffer_offset + i + 2);
+                }
+                index_offset += fv;
+                index_buffer_offset += fv;
+
+                // per-face material
+                // shapes[s].mesh.material_ids[f];
+            }
+        }
+
+        m_device->debugMarkerPush(fmt::format("Loading mesh {}...", meshPath));
+
+        gpu::BufferHandle vertexBufferHandle;
+        gpu::BufferHandle indexBufferHandle;
+
+        // Vertex buffer
+        vertexBufferHandle = m_device->makeBuffer({ .type = gpu::BufferType::VertexBuffer, .usage = gpu::Usage::Default, .debugName = fmt::format("{}_vertexBuffer", meshPath) });
+        m_device->writeBuffer(vertexBufferHandle, vertices.size() * sizeof(render::PositionNormalTexcoordVertex), vertices.data());
+
+        // Triangle indices
+        indexBufferHandle = m_device->makeBuffer({ .type = gpu::BufferType::IndexBuffer, .usage = gpu::Usage::Default, .format = gpu::GpuFormat::Uint32_TYPELESS, .debugName = fmt::format("{}_indexBuffer", meshPath) });
+        m_device->writeBuffer(indexBufferHandle, indices.size() * sizeof(uint32_t), indices.data());
+
+        // the VAO is tied to the mesh, soooo
+        gpu::VertexAttributeDesc vDesc[] = {
+            {.name = "POSITION", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 0, .offset = offsetof(render::PositionNormalTexcoordVertex, position), .elementStride = sizeof(render::PositionNormalTexcoordVertex)},
+            {.name = "NORMAL", .format = gpu::GpuFormat::RGB8_TYPELESS, .bufferIndex = 1, .offset = offsetof(render::PositionNormalTexcoordVertex, normal), .elementStride = sizeof(render::PositionNormalTexcoordVertex)},
+            {.name = "TEXCOORD0", .format = gpu::GpuFormat::RG8_TYPELESS, .bufferIndex = 2, .offset = offsetof(render::PositionNormalTexcoordVertex, uv), .elementStride = sizeof(render::PositionNormalTexcoordVertex)}
+        };
+
+        gpu::InputLayoutHandle vertexLayoutHandle = m_device->createInputLayout(vDesc, sizeof(vDesc) / sizeof(vDesc[0]));
+
+        render::Mesh outputMesh{
+            .vertexBuffer = vertexBufferHandle,
+            .indexBuffer = indexBufferHandle,
+            .vertexLayout = vertexLayoutHandle,
+            .triangleCount = indices.size() / 3 // There are 3 vertices per triangle, so divide by 3
+        };
+
+        m_device->debugMarkerPop();
+
+        if (vertexBufferHandle.Get() != nullptr && indexBufferHandle.Get() != nullptr) {
+            // Cache
+            MeshTracker_t tracker = {
+                .vertexBuffer = vertexBufferHandle,
+                .indexBuffer = indexBufferHandle,
+                .vertexLayout = vertexLayoutHandle,
+                .mesh = outputMesh,
+            };
+            m_meshes.emplace(meshKey, tracker);
+            return outputMesh;
+        }
+        else {
+            LOG_WARNING("Failed to load mesh {}. Using error mesh...", meshPath);
+            return m_errorMesh.mesh;
+        }
+
+        LOG_WARNING("Failed to load mesh {}. Using error mesh...", meshPath);
+        return m_errorMesh.mesh;
     }
     gpu::IShader* AssetManager::fetchShader(const FetchShaderParams& params) {
         
@@ -165,11 +385,16 @@ void main()
             }
         }
 
+        m_device->debugMarkerPush(fmt::format("Loading shader {} , {}...", params.vertShader, params.fragShader));
+
         auto shaderHandle = m_device->makeShader({
-            .debugName = params.debugName,
             .VS {.byteCode = (uint8_t*)vertContents.c_str(), .entryFunc = params.vertShaderEntryFunction },
             .PS {.byteCode = (uint8_t*)fragContents.c_str(), .entryFunc = params.fragShaderEntryFunction },
-            .graphicsState = params.graphicsState });
+            .graphicsState = params.graphicsState,
+            .debugName = params.debugName,
+        });
+
+        m_device->debugMarkerPop();
 
         if (shaderHandle.Get() != nullptr) {
             // Cache
@@ -179,7 +404,7 @@ void main()
             return m_errorShader;
         }
     }
-    gpu::ITexture* AssetManager::fetchTexture(const std::string& texturePath, bool genMipmaps) {
+    gpu::ITexture* AssetManager::fetchTexture(const std::string& texturePath, const bool genMipmaps) {
 
         if (!m_intialisedDefaultAssets) {
             initialiseErrorData();
@@ -193,12 +418,15 @@ void main()
         int texWidth, texHeight, nrChannels;
         unsigned char* texData = stbi_load(fmt::format("{}/assets/textures/{}", m_applicationRootPath, texturePath).c_str(), &texWidth, &texHeight, &nrChannels, STBI_rgb_alpha);
 
+        m_device->debugMarkerPush(fmt::format("Loading texture {}...", texturePath));
         gpu::TextureHandle textureHandle = m_device->makeTexture({
             .width = (uint32_t)texWidth,
             .height = (uint32_t)texHeight,
             .generateMipmaps = genMipmaps,
             .type = gpu::TextureType::Texture2D,
+            .debugName = texturePath,
             }, texData);
+        m_device->debugMarkerPop();
 
         if (textureHandle.Get() != nullptr) {
             // Cache
--- src/engine/managers/asset_manager.hpp
+++ src/engine/managers/asset_manager.hpp
@@ -17,8 +17,6 @@ namespace managers {
         AssetManager(gpu::IDevice* device);
         ~AssetManager();
 
-        void unloadAssets();
-
         struct FetchShaderParams {
             gpu::GraphicsState graphicsState;
             std::string vertShader;
@@ -30,20 +28,32 @@ namespace managers {
         
         render::Mesh& fetchMesh(const std::string& meshPath);
         gpu::IShader* fetchShader(const FetchShaderParams& params);
-        gpu::ITexture* fetchTexture(const std::string& texturePath, bool genMipmaps = true);
+        gpu::ITexture* fetchTexture(const std::string& texturePath, const bool genMipmaps = true);
         
     private:
         void initialiseErrorData();
         std::string getExecutableDir();
 
     private:
+
+        // Keeps track of mesh data for caching and memory management purposes
+        class MeshTracker_t {
+        public:
+            // Keep the handles for free memory management
+            gpu::BufferHandle vertexBuffer;
+            gpu::BufferHandle indexBuffer;
+            gpu::InputLayoutHandle vertexLayout;
+            // Pointers, passed to "userland" code
+            render::Mesh mesh;
+        };
+
         std::unordered_map<std::string, gpu::ShaderHandle> m_shaders;
         std::unordered_map<std::string, gpu::TextureHandle> m_textures;
-        std::unordered_map<std::string, render::Mesh> m_meshes;
+        std::unordered_map<std::string, MeshTracker_t> m_meshes;
 
         gpu::ShaderHandle m_errorShader;
         gpu::TextureHandle m_errorTexture;
-        render::Mesh m_errorMesh;
+        MeshTracker_t m_errorMesh;
 
         bool m_intialisedDefaultAssets = false;
         gpu::IDevice* m_device;
