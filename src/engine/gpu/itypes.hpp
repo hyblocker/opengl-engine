cd73851f9425d34cb8c5d0f9f2271e775fb98cd9
Hyblocker <hyblocker@protonmail.com>
2025-02-05 22:07:12 +0100

    (feat): Bitwise ops for enums

--- src/engine/gpu/itypes.hpp
+++ src/engine/gpu/itypes.hpp
@@ -8,8 +8,17 @@ namespace gpu {
 	// Alias for native pointer to gpu memory, used to bind textures etc in cbuffers
 	typedef uint32_t GpuPtr;
 
-	// Primitive types and enums
+	// Macro taken from nvrhi
+	// https://github.com/NVIDIAGameWorks/nvrhi/blob/656bb06d35f6fe92bafaeab368a2d65da9bc4b58/include/nvrhi/nvrhi.h#L34C1-L40C74
+#define GPU_ENUM_CLASS_FLAG_OPERATORS(T) \
+    inline T operator | (T a, T b) { return T(uint32_t(a) | uint32_t(b)); } \
+    inline T operator & (T a, T b) { return T(uint32_t(a) & uint32_t(b)); } /* NOLINT(bugprone-macro-parentheses) */ \
+    inline T operator ~ (T a) { return T(~uint32_t(a)); } /* NOLINT(bugprone-macro-parentheses) */ \
+    inline bool operator !(T a) { return uint32_t(a) == 0; } \
+    inline bool operator ==(T a, uint32_t b) { return uint32_t(a) == b; } \
+    inline bool operator !=(T a, uint32_t b) { return uint32_t(a) != b; }
 
+	// Primitive types and enums
 	enum class GpuFormat : uint8_t {
 		Unknown,
 		Uint8_TYPELESS,
@@ -49,6 +58,7 @@ namespace gpu {
 		FlushEexplicit = 0x40,
 		Unsychronised = 0x80,
 	};
+	GPU_ENUM_CLASS_FLAG_OPERATORS(MapAccessFlags);
 
 	struct Rect {
 		uint32_t left = 0;
